f31bd8122e6737ec2e40faaa2066d3c97361f26c
L <ehimimaru@gmail.com>
2014-07-22 05:20:55 +0400

    Russian translate

---
+++ system/third_party/draftee/language/russian/lang.draftee.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['draftee_module_name'] = "DraftEE";
+$lang['draftee_module_description'] = "Модуль для работы с версионностью записей.";
+$lang['welcome'] = "Добро пожаловать в DraftEE";
